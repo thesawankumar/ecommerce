83e8b3622fee12db2ead429ffbdbdca3a698b586
Sawan Kumar <sawankushwaha222@gmail.com>
2025-08-19 16:36:56 +0530

    User model error solve

--- src/main/java/thesawan/in/ecommerce/model/User.java
+++ src/main/java/thesawan/in/ecommerce/model/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
